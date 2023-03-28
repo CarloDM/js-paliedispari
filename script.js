996ec22cdcc43acacbea40fa3cba2775d63e7be7
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-28 21:11:59 +0200

    pari e dispari wip

--- script.js
+++ script.js
@@ -2,38 +2,65 @@ console.log ('ciao')
 //  1 dividere le lettere della string 
 //  2 verificare se la string letto in un senso è uguale a leggerlo nel senso opposto
 // 3 ma come piu nello specifico?---uhm..
-
-let parola = prompt();
-
-console.log('la parola inserita è ', parola);
-
 // dividere la string dentro 
 
 // ci vuole un ciclo  per verificare lettera per lettera
 
-function palidroma(parolaV){
-  
-  let parolaFor = [];
-  let parolaInv = [];
+// let parola = prompt();
 
-  for (let i = 0; i < parolaV.length; i++) {
+// console.log('la parola inserita è ', parola);
 
-      parolaFor.push(parolaV[i])
-  }
+
+// function palidroma(parolaV){
   
-  for (let i = parolaV.length; i > 0; i--) {
+//   let parolaFor = [];
+//   let parolaInv = [];
+
+//   for (let i = 0; i < parolaV.length; i++) {
+
+//       parolaFor.push(parolaV[i])
+//   }
+  
+//   for (let i = parolaV.length; i > 0; i--) {
     
-      parolaInv.push(parolaV[i-1])
-  }
+//       parolaInv.push(parolaV[i-1])
+//   }
   
-  if (parolaFor.toString() === parolaInv.toString()){
-    console.warn('la parolaV è palidroma')
-    return true;
-  }else{
-    console.warn('la parola non è palidroma')
-    return false;
-  }
-  
+//   if (parolaFor.toString() === parolaInv.toString()) {
+//     console.warn('la parolaV è palidroma')
+//     return true;
+//   }else{
+//     console.warn('la parola non è palidroma')
+//     return false;
+//   }
+// }
+
+// console.log('palidroma', palidroma(parola))
+
+
+// --------------------------------------------------------------
+// ---------------------------------------------------------------
+
+
+const userChoice = 'dispari';
+const userNum = 4;
+const cpuNum = Math.floor(Math.random()*5 +1)
+console.log(userChoice)
+if (userChoice == 'pari'){
+    cpuChoice = 'dispari';
+}else {
+    cpuChoice = 'pari';
 }
 
-console.log('palidroma', palidroma(parola))
+console.log('utente è',userChoice, userNum);
+console.log('cpu è',cpuChoice,cpuNum);
+
+const sum = (userNum + cpuNum) % 2;
+
+console.log(sum)
+
+if (!sum) {
+  console.log('pari')
+} else {
+  console.log('dispari')
+}
