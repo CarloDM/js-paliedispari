0ab1f2afbca84aabde42293fd241d7c7f77b640f
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-28 17:08:44 +0200

    verifica palindroma work

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ console.log ('ciao')
 //  2 verificare se la string letto in un senso è uguale a leggerlo nel senso opposto
 // 3 ma come piu nello specifico?---uhm..
 
-let parola = 'kayak';
-let parolaInv ;
+let parola = prompt();
+let parolaFor = [];
+let parolaInv = [];
 
 console.log('la parola inserita è ', parola);
 
@@ -13,12 +14,12 @@ console.log('la parola inserita è ', parola);
 // ci vuole un ciclo while per verificare lettera per lettera
 
 function palindroma(parola){
-
 }
 
 for (let i = 0; i < parola.length; i++) {
-  
+
     console.log('for',parola[i])
+    parolaFor.push(parola[i])
   
 }
 
@@ -27,5 +28,15 @@ console.log(parola.length)
 for (let i = parola.length; i > 0; i--) {
   
     console.log('inv',parola[i-1])
-  
+    parolaInv.push(parola[i-1])
+}
+
+console.log('parolafor',parolaFor.toString())
+console.log('parolainv',parolaInv.toString())
+
+
+if (parolaFor.toString() === parolaInv.toString()){
+  console.warn('la parola è palindroma')
+}else{
+  console.warn('la parola non è palindroma')
 }
