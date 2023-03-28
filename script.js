61b10210de3c03b3c06d77c3813f16606e785ee3
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-28 16:53:28 +0200

    controllo inverso con 2 cicli for

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log ('ciao')
+//  1 dividere le lettere della string 
+//  2 verificare se la string letto in un senso è uguale a leggerlo nel senso opposto
+// 3 ma come piu nello specifico?---uhm..
+
+let parola = 'kayak';
+let parolaInv ;
+
+console.log('la parola inserita è ', parola);
+
+// dividere la string dentro 
+
+// ci vuole un ciclo while per verificare lettera per lettera
+
+function palindroma(parola){
+
+}
+
+for (let i = 0; i < parola.length; i++) {
+  
+    console.log('for',parola[i])
+  
+}
+
+console.log(parola.length)
+
+for (let i = parola.length; i > 0; i--) {
+  
+    console.log('inv',parola[i-1])
+  
+}
